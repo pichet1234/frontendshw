de7db15a3d7a50c4f0709b586dc61b0023821900
PichetDotK <146558868+pichet1234@users.noreply.github.com>
2025-06-11 16:30:25 +0000

    redir

---
+++ dist/demo1/browser/chunk-MTKTS7OC.js
@@ -0,0 +1,1 @@
+import"./chunk-GAL4ENT6.js";var o=[{path:"registertwo",loadComponent:()=>import("./chunk-FI6NENV3.js").then(t=>t.RegistertwoComponent)}];export{o as default};
---
+++ dist/demo1/browser/chunk-QPT6QXBV.js
@@ -0,0 +1,1 @@
+import"./chunk-GAL4ENT6.js";var t=[{path:"",loadComponent:()=>import("./chunk-N6SXVUCV.js").then(o=>o.DashboardComponent)}];export{t as default};
